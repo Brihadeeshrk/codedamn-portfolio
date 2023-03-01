4c7466c178b9c88a5c3b7893dd8c73a7134563bb
briha <hello@briha.live>
2023-03-02 02:09:42 +0530

    interests

--- src/components/Resume/Interests/index.tsx
+++ src/components/Resume/Interests/index.tsx
@@ -1,8 +1,22 @@
+import { Badge, Button, Flex, Text } from "@chakra-ui/react";
 import React from "react";
 
 type indexProps = {};
 
-const index: React.FC<indexProps> = () => {
-  return <div>Interests</div>;
+const interests = ["Web Development", "Cycling", "Formula1", "Excercise"];
+
+const Index: React.FC<indexProps> = () => {
+  return (
+    <Flex direction="column" justify="center" className="mt-4">
+      <Text className="font-bold text-xl mb-2">Education</Text>
+      <Flex className="rounded-lg">
+        {interests.map((item, index) => (
+          <Badge className="mr-2" key={index}>
+            {item}
+          </Badge>
+        ))}
+      </Flex>
+    </Flex>
+  );
 };
-export default index;
+export default Index;
