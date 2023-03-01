c58d1ba46056a21ff3e5427a1a79b00a892a80de
briha <hello@briha.live>
2023-03-02 02:05:58 +0530

    techskills

--- src/components/Resume/TechSkills/index.tsx
+++ src/components/Resume/TechSkills/index.tsx
@@ -1,8 +1,51 @@
+import { Flex, Text, Icon, Badge, Box } from "@chakra-ui/react";
 import React from "react";
+import { AiFillHtml5 } from "react-icons/ai";
+import { FaReact } from "react-icons/fa";
+import { DiCss3Full, DiJava, DiJavascript1 } from "react-icons/di";
+import {
+  SiTypescript,
+  SiNextdotjs,
+  SiGatsby,
+  SiPython,
+  SiFirebase,
+} from "react-icons/si";
 
 type indexProps = {};
 
-const index: React.FC<indexProps> = () => {
-  return <div>Tech Skills</div>;
+export type Skill = {
+  name: string;
+  icon: typeof Icon.arguments;
 };
-export default index;
+
+const Skills: Skill[] = [
+  // { name: "HTML", icon: AiFillHtml5 },
+  // { name: "CSS", icon: DiCss3Full },
+  { name: "JavaScript", icon: DiJavascript1 },
+  // { name: "Java", icon: DiJava },
+  // { name: "Python", icon: SiPython },
+  // { name: "React", icon: FaReact },
+  { name: "Firebase", icon: SiFirebase },
+  { name: "TypeScript", icon: SiTypescript },
+  { name: "Next", icon: SiNextdotjs },
+  // { name: "Gatsby", icon: SiGatsby },
+];
+
+const Index: React.FC<indexProps> = () => {
+  return (
+    <Flex width="100%" direction="column" justify="center" className="mt-4">
+      <Text className="font-bold text-xl mb-2">Tech Skills</Text>
+      <Flex direction="row" className="rounded-lg mb-2 mt-2 " maxWidth="728px">
+        {Skills.map((item, index) => (
+          <Box key={index}>
+            <Badge className="mr-2">
+              <Icon as={item.icon} height="30px" className="mr-1" />
+              {item.name}
+            </Badge>
+          </Box>
+        ))}
+      </Flex>
+    </Flex>
+  );
+};
+export default Index;
