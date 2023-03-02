f01bc8421bf92b6e4f4247827c653809b143f0ce
briha <hello@briha.live>
2023-03-02 12:07:46 +0530

    certificates ok

---
+++ src/components/Portfolio/Certificates/CertificateCard.tsx
@@ -0,0 +1,41 @@
+import { Badge, Box, Flex, Icon, Image, Text } from "@chakra-ui/react";
+import React from "react";
+import { Certificate } from "./index";
+
+type ProjectCardProps = {
+  CertificateDetails: Certificate;
+};
+
+const ProjectCard: React.FC<ProjectCardProps> = ({ CertificateDetails }) => {
+  return (
+    <Flex
+      width="100%"
+      className="bg-slate-100 p-2 mt-2 rounded-lg border-2 border-slate-200"
+      direction="column"
+    >
+      <Flex align="start" className="mb-2">
+        <Icon as={CertificateDetails.icon} height="20%" width="10%" />
+      </Flex>
+      <Flex align="start" direction="column">
+        <Text className="font-bold text-sm">{CertificateDetails.title}</Text>
+        <Text className="text-xs text-slate-500">
+          Issued on {CertificateDetails.issueDate}
+        </Text>
+        <Text className="font-bold text-xs text-slate-500 mt-4 cursor-pointer">
+          See credentials
+        </Text>
+      </Flex>
+      <Flex align="center">
+        {/* {ProjectStats.stack.map((item, index) => (
+          <Badge key={index}>
+            <Flex direction="row" align="center" className="">
+              <Icon as={item.icon} className="mr-1" />
+              <Text className="text-xs">{item.name}</Text>
+            </Flex>
+          </Badge>
+        ))} */}
+      </Flex>
+    </Flex>
+  );
+};
+export default ProjectCard;
--- src/components/Portfolio/Certificates/index.tsx
+++ src/components/Portfolio/Certificates/index.tsx
@@ -1,8 +1,70 @@
 import React from "react";
+import { Flex, Icon, Text } from "@chakra-ui/react";
+
+import { AiFillHtml5 } from "react-icons/ai";
+import { FaReact } from "react-icons/fa";
+import { DiCss3Full, DiJava, DiJavascript1 } from "react-icons/di";
+import {
+  SiTypescript,
+  SiNextdotjs,
+  SiGatsby,
+  SiPython,
+  SiFirebase,
+} from "react-icons/si";
+import CertificateCard from "./CertificateCard";
 
 type indexProps = {};
 
+export type Certificate = {
+  icon: typeof Icon.arguments;
+  title: string;
+  issueDate: string;
+};
+
+const Certificates: Certificate[] = [
+  {
+    icon: AiFillHtml5,
+    title: "HTML/CSS",
+    issueDate: "Dec 16th, 2022",
+  },
+  {
+    icon: FaReact,
+    title: "React",
+    issueDate: "Dec 16th, 2022",
+  },
+  {
+    icon: AiFillHtml5,
+    title: "HTML/CSS",
+    issueDate: "Dec 16th, 2022",
+  },
+  {
+    icon: FaReact,
+    title: "React",
+    issueDate: "Dec 16th, 2022",
+  },
+];
+
 const index: React.FC<indexProps> = () => {
-  return <div>Certificates</div>;
+  return (
+    <Flex direction="column" justify="center" className="mt-8" width="100%">
+      <Flex justify="space-between" align="center">
+        <Text className="font-bold text-xl mb-2">Certificates</Text>
+        <Text className="font-bold text-xs mb-2 text-indigo-700">
+          Add new certificate
+        </Text>
+      </Flex>
+
+      <Flex direction="row" align="center" justify="center" width="100%">
+        <Flex width="50%" direction="column" className="mr-2">
+          <CertificateCard CertificateDetails={Certificates[0]} />
+          <CertificateCard CertificateDetails={Certificates[2]} />
+        </Flex>
+        <Flex width="50%" direction="column">
+          <CertificateCard CertificateDetails={Certificates[1]} />
+          <CertificateCard CertificateDetails={Certificates[3]} />
+        </Flex>
+      </Flex>
+    </Flex>
+  );
 };
 export default index;
